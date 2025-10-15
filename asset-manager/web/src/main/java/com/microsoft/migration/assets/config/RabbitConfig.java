b51cb80cf542cba378a33a550b57555db2239897
Yi Liu <yiliu6@microsoft.com>
2025-10-15 10:54:12 +0800

    simply rabbitmq config

--- asset-manager/web/src/main/java/com/microsoft/migration/assets/config/RabbitConfig.java
+++ asset-manager/web/src/main/java/com/microsoft/migration/assets/config/RabbitConfig.java
@@ -1,9 +1,6 @@
 package com.microsoft.migration.assets.config;
 
 import org.springframework.amqp.core.AcknowledgeMode;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -18,42 +15,13 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
     public static final String IMAGE_PROCESSING_QUEUE = "image-processing";
 
-    // Dead letter queue configuration for the retry mechanism
-    public static final String RETRY_EXCHANGE = "image-processing.retry";
-    public static final String RETRY_QUEUE = "image-processing.retry";
-    public static final String RETRY_ROUTING_KEY = "retry";
-    public static final int RETRY_DELAY_MS = 60000; // 1 minute delay
-
     @Bean
     public Queue imageProcessingQueue() {
-        return QueueBuilder.durable(IMAGE_PROCESSING_QUEUE
-)
-                .withArgument("x-dead-letter-exchange", RETRY_EXCHANGE)
-                .withArgument("x-dead-letter-routing-key", RETRY_ROUTING_KEY)
-                .build();
-    }
-
-    @Bean
-    public Queue retryQueue() {
-        return QueueBuilder.durable(RETRY_QUEUE)
-                .withArgument("x-dead-letter-exchange", "")
-                .withArgument("x-dead-letter-routing-key", IMAGE_PROCESSING_QUEUE
-        )
-                .withArgument("x-message-ttl", RETRY_DELAY_MS)
-                .build();
-    }
-
-    @Bean
-    public DirectExchange retryExchange() {
-        return new DirectExchange(RETRY_EXCHANGE);
-    }
-
-    @Bean
-    public Binding retryBinding() {
-        return BindingBuilder
-                .bind(retryQueue())
-                .to(retryExchange())
-                .with(RETRY_ROUTING_KEY);
+        return QueueBuilder.durable(IMAGE_PROCESSING_QUEUE)
+        .ttl(300000) // Message TTL of 5 minutes
+        .expires(86400000) // Queue expires after 24 hours of inactivity
+        .maxLength(1000) // Max 1000 messages in the queue
+        .build();
     }
 
     @Bean
@@ -69,7 +37,6 @@ public class RabbitConfig {
         configurer.configure(factory, connectionFactory);
         factory.setMessageConverter(jsonMessageConverter());
         factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-        factory.setDefaultRequeueRejected(false);
         return factory;
     }
 }
